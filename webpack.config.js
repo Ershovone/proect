bef038380576c38b4b7d80ff2c5ffb1eceb2cb57
DBriksin <briksin_isd@mail.ru>
2017-10-01 18:05:26 +0300

    new project

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/index',
+
+
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'app.js',
+        publicPath: '/public'
+    },
+
+    devServer: {
+        host: '127.0.0.1',
+        port: 4000,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+        },
+        contentBase: path.join(__dirname, 'public'),
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets:[ 'es2015', 'react', 'stage-2' ]
+            }
+        },
+        {
+            test: /\.scss$/,
+            use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader', 'sass-loader'],
+            })),
+        }]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css', '.scss'],
+        modules: [
+            'node_modules'
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css')
+    ],
+
+    devtool: 'cheap-eval-source-map' // remove for build
+};
+
